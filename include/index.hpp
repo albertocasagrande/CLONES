aeaef216f30d6e1c1267d3c1b786a364d07b563a
Alberto Casagrande <alberto.casagrande@uniud.it>
2025-10-11 15:41:01 +0200

    Enhanced `Index` - index shuffling now supports progress bar - implemented `insert(const KEY&, VALUE&&)` - implemented `boot_up_tours(RANDOM_GENERATOR&, ...)` - implemented `boot_up_tours(const int seed, ...)` - implemented `shuffle(const int seed, ...)`

--- include/index.hpp
+++ include/index.hpp
@@ -2,8 +2,8 @@
  * @file index.hpp
  * @author Alberto Casagrande (alberto.casagrande@uniud.it)
  * @brief Defines index
- * @version 1.0
- * @date 2025-09-17
+ * @version 1.1
+ * @date 2025-10-11
  *
  * @copyright Copyright (c) 2023-2025
  *
@@ -54,9 +54,9 @@ namespace Archive
  * of values, named classes, whose union consists in all values
  * representable by the type.
  *
- * The default is partition of a type consists of singleton 
+ * The default is partition of a type consists of singleton
  * classes.
- * 
+ *
  * @tparam TYPE is the type for which this structure defines
  *      the partition
  */
@@ -66,7 +66,7 @@ struct partition
     /**
      * @brief Return a list of the values in a class
      *
-     * @param value is a representant of the class that is
+     * @param[in] value is a representant of the class that is
      *      aimed
      * @return a list of the values in the class including
      *      `value`
@@ -87,7 +87,7 @@ struct partition
  * of key buckets and a map file containing the map key-bucket.
  * This class is a base class for both `IndexBuilder` and
  * `IndexReader`.
- * 
+ *
  * @tparam KEY is a type of the index keys
  */
 template<class KEY>
@@ -102,8 +102,8 @@ class IndexBase
 protected:
     /**
      * @brief Set the prefix of the index bucket filenames
-     * 
-     * @param bucket_prefix is the new prefix of bucket filename 
+     *
+     * @param[in] bucket_prefix is the new prefix of bucket filename
      */
     inline void set_bucket_prefix(const std::string& bucket_prefix)
     {
@@ -120,10 +120,10 @@ public:
 
     /**
      * @brief A constructor
-     * 
-     * @param index_path is the path of the index directory
-     * @param cache_size is the index cache size in bytes
-     * @param bucket_prefix is the prefix of the index bucket
+     *
+     * @param[in] index_path is the path of the index directory
+     * @param[in] cache_size is the index cache size in bytes
+     * @param[in] bucket_prefix is the prefix of the index bucket
      *      filenames
      */
     IndexBase(const std::filesystem::path index_path,
@@ -139,11 +139,11 @@ public:
 
     /**
      * @brief Get the path of a bucket file
-     * 
-     * This method returns the path of the bucket file associated to 
+     *
+     * This method returns the path of the bucket file associated to
      * a key.
-     * 
-     * @param key is the key associated to the aimed bucket file path
+     *
+     * @param[in] key is the key associated to the aimed bucket file path
      * @return the path of the bucket file associated to `key`
      */
     std::filesystem::path get_bucket_path(const KEY& key) const
@@ -157,7 +157,7 @@ public:
 
     /**
      * @brief Get the prefix of the bucket filenames
-     * 
+     *
      * @return the prefix of the bucket filenames
      */
     inline const std::string& get_bucket_prefix() const
@@ -167,12 +167,12 @@ public:
 
     /**
      * @brief Get the filename of the index map file
-     * 
+     *
      * Any index is saved in a directory as a file mapping
      * any key to a bucket, named *map file*, and the
      * corresponding buckets.
      * This method returns the filename of the map file.
-     * 
+     *
      * @return the filename of the index map file
      */
     inline static constexpr std::string map_filename()
@@ -182,7 +182,7 @@ public:
 
     /**
      * @brief Get the index map file path
-     * 
+     *
      * Any index is saved in a directory as a file mapping
      * any key to a bucket, named *map file*, and the
      * corresponding buckets.
@@ -198,7 +198,7 @@ public:
 
     /**
      * @brief Get the index directory path
-     * 
+     *
      * @return the index directory path
      */
     inline const std::filesystem::path& get_path() const
@@ -208,7 +208,7 @@ public:
 
     /**
      * @brief Get the index cache size
-     * 
+     *
      * @return the index cache size in bytes
      */
     inline const size_t& get_cache_size() const
@@ -218,9 +218,9 @@ public:
 
     /**
      * @brief Get the cache size per index bucket
-     * 
-     * @param num_of_keys is the number of index keys
-     * @return the cache size per index bucket in bytes if the index 
+     *
+     * @param[in] num_of_keys is the number of index keys
+     * @return the cache size per index bucket in bytes if the index
      *      has `num_of_keys` keys
      */
     inline size_t cache_size_per_bucket(const size_t num_of_keys) const
@@ -271,16 +271,16 @@ private:
 
     /**
      * @brief Associated a new bucket to a key
-     * 
+     *
      * This method associated a new bucket to a key in the
      * key-bucket map. If the corresponding bucket file
      * already exists or new association cannot be added
      * to the index map, a `std::runtime_error` is thrown.
-     * 
-     * @param key is the key to which the new bucket is
+     *
+     * @param[in] key is the key to which the new bucket is
      *      associated
      * @return an iterator of the key-bucket map referring
-     *      to the just inserted pair `key`-`key`'s bucket 
+     *      to the just inserted pair `key`-`key`'s bucket
      */
     BucketMapType::iterator add_bucket_for(const KEY& key)
     {
@@ -316,10 +316,10 @@ private:
 public:
     /**
      * @brief Construct a new index builder
-     * 
-     * @param index_path is the path to the directory storing the index
-     * @param cache_size is the write cache size in bytes
-     * @param bucket_prefix is the prefix of the bucket filenames
+     *
+     * @param[in] index_path is the path to the directory storing the index
+     * @param[in] cache_size is the write cache size in bytes
+     * @param[in] bucket_prefix is the prefix of the bucket filenames
      */
     explicit IndexBuilder(const std::filesystem::path index_path,
                           const size_t cache_size = 10000000,
@@ -345,8 +345,8 @@ public:
     /**
      * @brief Insert a value in the index
      *
-     * @param key is the key of the value
-     * @param value is the value to be inserted
+     * @param[in] key is the key of the value
+     * @param[in] value is the value to be inserted
      */
     void insert(const KEY& key, const VALUE& value)
     {
@@ -361,13 +361,13 @@ public:
     /**
      * @brief Insert a value in the index
      *
-     * @param key is the key of the value
-     * @param value is the value to be inserted
+     * @param[in,out] key is the key of the value
+     * @param[in,out] value is the value to be inserted
      */
     void insert(KEY&& key, VALUE&& value)
     {
-        auto found = buckets->find(key);
-        if (found == buckets->end()) {
+        auto found = buckets.find(key);
+        if (found == buckets.end()) {
             found = add_bucket_for(key);
         }
 
@@ -375,30 +375,107 @@ public:
     }
 
     /**
-     * @brief Shuffle the values in the index buckets.
+     * @brief Insert a value in the index
+     *
+     * @param[in] key is the key of the value
+     * @param[in,out] value is the value to be inserted
+     */
+    void insert(const KEY& key, VALUE&& value)
+    {
+        auto found = buckets.find(key);
+        if (found == buckets.end()) {
+            found = add_bucket_for(key);
+        }
+
+        found->second.push_back(std::move(value));
+    }
+
+    /**
+     * @brief Shuffle the values in the index buckets
+     *
+     * @tparam ARGS are the types of the parameters
+     * @param[in, out] args are the parameters
+     */
+    template <typename... ARGS>
+    requires (!std::same_as<std::remove_cvref_t<
+                typename std::tuple_element<sizeof...(ARGS) - 1, std::tuple<ARGS...>>::type
+                >, UI::ProgressBar>)
+    inline void shuffle(ARGS... args)
+    {
+        UI::ProgressBar progress_bar;
+
+        shuffle(args..., progress_bar);
+    }
+
+    /**
+     * @brief Shuffle the values in the index buckets
+     *
+     * @tparam RANDOM_GENERATOR is a random number generator type
+     * @tparam ARGS are the types of the remaining parameters
+     * @param[in] seed is the seed of a random number generator
+     * @param[in, out] args are the remaining parameters
+     */
+    template<typename RANDOM_GENERATOR, typename... ARGS>
+    inline void shuffle(const int seed, ARGS... args)
+    {
+        RANDOM_GENERATOR generator(seed);
+
+        shuffle(generator, args...);
+    }
+
+    /**
+     * @brief Shuffle the values in the index buckets
+     *
+     * @tparam RANDOM_GENERATOR is a random number generator type
+     * @param[in, out] random_generator is a random number generator
+     * @param[in, out] progress_bar is a progress bar
+     */
+    template<typename RANDOM_GENERATOR>
+    inline void shuffle(RANDOM_GENERATOR& random_generator,
+                        UI::ProgressBar& progress_bar)
+    {
+        shuffle(random_generator, std::filesystem::temp_directory_path(),
+                progress_bar);
+    }
+
+    /**
+     * @brief Shuffle the values in the index buckets
      *
      * @tparam RANDOM_GENERATOR is a random number generator type
      * @param[in, out] random_generator is a random number generator
      * @param[in] tmp_dir is the path of the temporary files
+     * @param[in, out] progress_bar is a progress bar
      */
     template<typename RANDOM_GENERATOR>
     void shuffle(RANDOM_GENERATOR& random_generator,
-                 const std::filesystem::path tmp_dir = std::filesystem::temp_directory_path())
+                 const std::filesystem::path tmp_dir,
+                 UI::ProgressBar& progress_bar)
     {
+        progress_bar.set_message("Shuffling index");
+
         // flush the cache of all the buckets and free as much memory as possible
         for (auto& [key, bucket]: buckets) {
+            progress_bar.update_elapsed_time();
+
             bucket.flush();
         }
-        
+
+        size_t progress{0};
         // use all the cache and sequentially shuffle each bucket
         for (auto& [key, bucket]: buckets) {
-            bucket.shuffle(random_generator, this->get_cache_size(), tmp_dir);
+            bucket.shuffle(random_generator, this->get_cache_size(), tmp_dir,
+                           progress_bar);
+
+            progress_bar.set_progress(100*(++progress)
+                                        /(buckets.size()+1));
         }
+
+        progress_bar.set_progress(100, "Index shuffled");
     }
 
     /**
      * @brief Save the map file on disk
-     * 
+     *
      * This method saves the map file in the index directory
      */
     void save_map_on_disk()
@@ -424,7 +501,7 @@ public:
 
     /**
      * @brief Destroy the Index Builder
-     * 
+     *
      * This method saves the index map file in the index directory
      * and destroys the index builder object.
      */
@@ -496,12 +573,12 @@ private:
 
     /**
      * @brief Add a tour iterator to the key-bucket iterator map
-     * 
+     *
      * This method associates a key with a random tour iterator over the bucket
      * that corresponds to the key in the key-bucket iterator map. If the new
      * association cannot be added to the map, a `std::runtime_error` is thrown.
-     * 
-     * @param key is the key associated to the bucket whose random tour iterator
+     *
+     * @param[in] key is the key associated to the bucket whose random tour iterator
      *      must be added to the key-bucket iterator map
      * @return an iterator to the added association in the key-bucket iterator map
      */
@@ -536,16 +613,15 @@ public:
 
     /**
      * @brief A constructor
-     * 
-     * @param index_path is the path of the index directory
-     * @param cache_size is the read cache size in bytes
+     *
+     * @param[in] index_path is the path of the index directory
+     * @param[in] cache_size is the read cache size in bytes
      */
     IndexReader(const std::filesystem::path index_path,
                 const size_t cache_size = 10000000):
         IndexBase<KEY>{index_path, cache_size},
         buckets{}, bucket_iterators{}
     {
-
         if (!std::filesystem::exists(index_path)) {
             std::ostringstream oss;
 
@@ -553,7 +629,7 @@ public:
                 << "\" does not exist.";
             throw std::domain_error(oss.str());
         }
-    
+
         if (!std::filesystem::is_directory(index_path)) {
             std::ostringstream oss;
 
@@ -601,7 +677,7 @@ public:
 
     /**
      * @brief Get the index keys
-     * 
+     *
      * @return a vector containing the index keys
      */
     std::vector<KEY> get_keys() const
@@ -621,7 +697,7 @@ public:
      *
      * This method returns the bucket of values associated to a key.
      *
-     * @param key is the searched bucket's key
+     * @param[in] key is the searched bucket's key
      * @return a constant reference to the bucket of values
      *    associated to `key`.
      */
@@ -633,7 +709,7 @@ public:
     /**
      * @brief Count the extractable values for a key
      *
-     * @param key is a key
+     * @param[in] key is a key
      * @return the number of extractable values associated to `key`
      */
     size_t extractable_for(const KEY& key) const
@@ -654,7 +730,7 @@ public:
     /**
      * @brief Count the values associated to a key
      *
-     * @param key is the searched key
+     * @param[in] key is the searched key
      * @return the number of values associated to `key`
      */
     size_t num_of_values(const KEY& key) const
@@ -669,12 +745,76 @@ public:
 
     /**
      * @brief Return the number of keys in the index
-     * 
-     * @return the number of keys in the index 
+     *
+     * @return the number of keys in the index
      */
     inline size_t num_of_keys() const
     {
         return buckets.size();
+    }
+
+    /**
+     * @brief Initialise bucket tours and caches some data
+     *
+     * This method boots up bucket tours randomizing the tour's
+     * initial position and loading their first chunk.
+     *
+     * @param[in,out] generator is a random number generator
+     * @param[in,out] progress_bar is a progress bar
+     */
+    void boot_up_tours(RANDOM_GENERATOR& generator,
+                       UI::ProgressBar& progress_bar)
+    {
+        progress_bar.set_message("Booting context index");
+
+        size_t booted_up{0};
+        for (const auto& [key, bucket]: buckets) {
+            auto found_it = bucket_iterators.find(key);
+
+            if (found_it == bucket_iterators.end()) {
+                found_it = add_tour_iterator_for(generator, key);
+            }
+
+            progress_bar.set_progress((100*(++booted_up))/buckets.size());
+        }
+    }
+
+    /**
+     * @brief Initialise bucket tours and caches some data
+     *
+     * This method boots up bucket tours randomizing the tour's
+     * initial position and loading their first chunk.
+     *
+     * @tparam ARGS are the types of the remaining parameters
+     * @param[in] seed is the seed of a random number generator
+     * @param[in, out] args are the remaining parameters
+     */
+    template<typename... ARGS>
+    inline void boot_up_tours(const int seed, ARGS... args)
+    {
+        RANDOM_GENERATOR generator(seed);
+
+        boot_up_tours(generator, args...);
+    }
+
+    /**
+     * @brief Initialise bucket tours and caches some data
+     *
+     * This method boots up bucket tours randomizing the tour's
+     * initial position and loading their first chunk.
+     *
+     * @tparam ARGS are the types of the parameters
+     * @param[in, out] args are the parameters
+     */
+    template <typename... ARGS>
+    requires (!std::same_as<std::remove_cvref_t<
+                typename std::tuple_element<sizeof...(ARGS) - 1, std::tuple<ARGS...>>::type
+                >, UI::ProgressBar>)
+    inline void boot_up_tours(ARGS... args)
+    {
+        UI::ProgressBar progress_bar;
+
+        boot_up_tours(args..., progress_bar);
     }
 
     /**
@@ -687,15 +827,15 @@ public:
      * If all values associated to the specified key have been
      * already extracted, a `std::runtime_error` is thrown.
      *
-     * @param generator is a random number generator
-     * @param key is a key
+     * @param[in,out] generator is a random number generator
+     * @param[in] key is a key
      * @return a random value associated to `key`
      */
     VALUE extract(RANDOM_GENERATOR& generator, const KEY& key)
     {
         auto found_it = bucket_iterators.find(key);
 
-        if (found_it==bucket_iterators.end()) {
+        if (found_it == bucket_iterators.end()) {
             found_it = add_tour_iterator_for(generator, key);
         }
 
@@ -720,8 +860,8 @@ public:
      * uniform distribution. The choosen value is *not* removed from
      * the index and can be choosen another time.
      *
-     * @param generator is the random number generator
-     * @param key is a key
+     * @param[in,out] generator is the random number generator
+     * @param[in] key is a key
      * @return a random value associated to `key`
      */
     template<typename VALUE2=VALUE,
@@ -735,7 +875,7 @@ public:
     /**
      * @brief Count the values in associated to a key class
      *
-     * @param key is a representant of the class whose
+     * @param[in] key is a representant of the class whose
      *      corresponding values are counted
      * @return the number of values associated to the class
      *      of `key`
@@ -759,8 +899,8 @@ public:
      * @brief Count the extractable values from a key class
      *
      * @tparam KEY_PARTITION is a partition of key type
-     * @param key is the a representant of the searched key class
-     * @return the number of extractable values associated to the 
+     * @param[in] key is the a representant of the searched key class
+     * @return the number of extractable values associated to the
      *      class of `key`
      */
     template<typename KEY_PARTITION=partition<KEY>>
@@ -785,8 +925,8 @@ public:
      * already extracted, a `std::runtime_error` is thrown.
      *
      * @tparam KEY_PARTITION is a partition of key type
-     * @param generator is the random number generator
-     * @param key is a representative of the key class from which
+     * @param[in,out] generator is the random number generator
+     * @param[in] key is a representative of the key class from which
      *      a value must be extracted
      * @return a pair whose first element is one of the keys in the
      *      class of `key`. The second element is one of the
@@ -832,8 +972,8 @@ public:
      * from the index and can be choosen another time.
      *
      * @tparam KEY_PARTITION is a partition of key type
-     * @param generator is the random number generator
-     * @param key is a key
+     * @param[in,out] generator is the random number generator
+     * @param[in] key is a key
      * @return a pair whose first element is one of the keys in the
      *      class of `key`. The second element is one of the
      *      values associated to the first element
